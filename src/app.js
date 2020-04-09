c7101b8228c0842a81a88ac28b1845186670ba85
Kay Johansen <kay-johansen@pluralsight.com>
2020-04-08 18:09:23 -0600

    Implemented module 3

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.urlencoded({extended: true}))
 
 const accountData = fs.readFileSync('src/json/accounts.json', {encoding: 'UTF8'})
 const accounts = JSON.parse(accountData)
@@ -19,5 +20,23 @@ app.get('/savings', (req,res) => res.render('account', {account: accounts.saving
 app.get('/checking', (req,res) => res.render('account', {account: accounts.checking}))
 app.get('/credit', (req,res) => res.render('account', {account: accounts.credit}))
 app.get('/profile', (req,res) => res.render('profile', {user: users[0]}))
+app.get('/transfer', (req,res) => res.render('transfer'))
+app.post('/transfer', (req,res) => {
+    var amount = parseInt(req.body.amount)
+    accounts[req.body.from].balance -= amount
+    accounts[req.body.to].balance += amount
+    var accountsJSON = JSON.stringify(accounts)
+    fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accountsJSON, 'utf8')
+    res.render('transfer', {message:'Transfer Completed'})
+})
+app.get('/payment', (req,res) => res.render('payment', {account: accounts.credit}))
+app.post('/payment', (req,res) => {
+    var amount = parseInt(req.body.amount)
+    accounts.credit.balance -= amount
+    accounts.credit.available += amount
+    var accountsJSON = JSON.stringify(accounts)
+    fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accountsJSON, 'utf8')
+    res.render('payment', {message: 'Payment Successful', account: accounts.credit})
+})
 
 app.listen(3000, () => console.log('PS Project Running on port 3000!'))
